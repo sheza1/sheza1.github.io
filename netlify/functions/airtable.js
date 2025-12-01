ab3a4b3aca0687c8634a58b681877242f0f95a37
Sheza Akhtar <82141635+sheza1@users.noreply.github.com>
2025-12-01 15:21:59 +0500

    Create airtable.js

---
+++ netlify/functions/airtable.js
@@ -0,0 +1,28 @@
+export async function handler() {
+  const AIRTABLE_TOKEN = process.env.AIRTABLE_TOKEN;
+  const BASE_ID = "appte9MwDwd1p6Bu5";
+  const TABLE_ID = "tblioq7HEm4Z39yt0";
+
+  const url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_ID}`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_TOKEN}`
+      }
+    });
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+}
